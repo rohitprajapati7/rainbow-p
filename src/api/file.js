b34b09365e1d0137fc4d243fafe4940a55bea762
rohitprajapati7 <prajapatirohitofficial@gmail.com>
2023-05-13 13:31:31 +0530

    fetch images from google drive

--- src/api/file.js
+++ src/api/file.js
@@ -1,28 +1,41 @@
-import axios from 'axios';
+import axios from "axios";
 
-const apiKey = 'AIzaSyD_QbaQq8Lcq2GCxmO_uKSBrkk8P7-6NRM';
-const fileId = '1TlB45JuqOSj-V5eCk0WF3MvkXg8zHlxS';
+const apiKey = "AIzaSyD_QbaQq8Lcq2GCxmO_uKSBrkk8P7-6NRM";
 
 // need to get only folderId (from folder we can get all the videos)
-const folderId = '1TlB45JuqOSj-V5eCk0WF3MvkXg8zHlxS'
+const videoFolderId = "1TlB45JuqOSj-V5eCk0WF3MvkXg8zHlxS";
+const imageFolderId = "1v6mFtLW3weF1nUt43XZ7vsGrLdIRgcyB";
 const endpoint = `https://www.googleapis.com/drive/v3/files`;
 
-const params = {
-    q: `'${folderId}' in parents and mimeType='video/mp4'`,
-  key: apiKey,
+export const fetchVideos = async () => {
+
+  const params = {
+    q: `'${videoFolderId}' in parents and mimeType='video/mp4'`,
+    key: apiKey,
+  };
+
+  try {
+    const response = await axios.get(endpoint, { params });
+    console.log("my response=", response);
+    return response?.data;
+  } catch (error) {
+    console.log("error =", error);
+    throw error;
+  }
 };
 
-export const fetchVideos = async() => {
-    try {
-        const response = await axios.get(endpoint, {params})
-        console.log('my response=', response);
-        return response?.data;
-    } catch (error) {
-        console.log('error =', error);
-        throw error;
-    }
-}
+export const fetchImages = async () => {
 
-// axios.get(endpoint, { params })
-//   .then(response => console.log(response.data))
-//   .catch(error => console.error('error',error));
+  const params = {
+    q: `'${imageFolderId}' in parents and mimeType contains 'image/'`,
+    key: apiKey,
+  };
+
+  try {
+    const response = await axios.get(endpoint, { params });
+    return response?.data;
+  } catch (error) {
+    console.log("error =", error);
+    throw error;
+  }
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 import Form from "../components/Form";
 import Team from "../components/Team";
 import Footer from "../components/Footer";
@@ -7,28 +5,12 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import SmallBanner from "../components/SmallBanner";
 import ContentEvent from "../components/ContentEvent";
+import VideoGallery from "../components/VideoGallery";
 import RainbowEventGroup from "../components/RainbowEventGroup";
 
-import { fetchVideos } from "../api/file";
-
 export default function Home() {
-  const [videoList, setVideoList] = useState([]);
-
-  const getVideos = async () => {
-    try {
-      const data = await fetchVideos();
-      setVideoList(data?.files);
-    } catch (error) {
-      console.log("error while fetching videos =", error);
-    }
-  };
-
-  useEffect(() => {
-    getVideos();
-  }, []);
-
   return (
-    <main className={``}>
+    <main>
       <div className="">
         <Header />
         <div className="main-slider-part relative">
@@ -46,27 +28,16 @@ export default function Home() {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 flex">
           <RainbowEventGroup />
         </div>
+        <div>
+          {/* will ad it later */}
+          {/* <VideoGallery /> */}
+        </div>
         <div className="team-section">
           <div className=" mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pt-8 pb-16 ">
             <Team />
           </div>
         </div>
-
         <Footer />
-        <div>
-          {videoList.length > 0 &&
-            videoList.map((res) => (
-              <div className="text-red-500" key={res?.id}>
-                <h1 className="text-red-600">{res.name}</h1>
-                <video
-                  src={`https://drive.google.com/uc?export=download&id=${res?.id}`}
-                  width={250}
-                  height={200}
-                  controls
-                ></video>
-              </div>
-            ))}
-        </div>
       </div>
     </main>
   );
